4f2aabb092266b43e87140a1a18f4a6c5160cb17
Adithya K B <adithyakb93@gmail.com>
2024-05-02 21:26:01 +0530

    feat: Add modal to show specific character details

--- components/charactercard.tsx
+++ components/charactercard.tsx
@@ -1,14 +1,16 @@
-import { Image, StyleSheet, Pressable } from "react-native";
+import { Image, StyleSheet, Pressable, Modal, Button } from "react-native";
 import { Text, View } from "./themed";
+import { useState } from "react";
 
 interface CharacterCardProps {
   character: info;
 }
 
 export default function CharacterCard({ character }: CharacterCardProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={styles.container}>
-      <Pressable>
+      <Pressable onPress={() => setIsModalVisible(true)}>
         <Image
           style={styles.image}
           source={{
@@ -20,6 +22,35 @@ export default function CharacterCard({ character }: CharacterCardProps) {
         />
         <Text style={styles.name}>{character.fullName}</Text>
       </Pressable>
+      <Modal
+        visible={isModalVisible}
+        animationType="fade"
+        onRequestClose={() => {
+          setIsModalVisible(false);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modal}>
+            <Image
+              style={[styles.image, { borderColor: "red" }]}
+              source={{
+                uri: character.imageUrl,
+                width: 200,
+                height: 200,
+              }}
+              alt={character.fullName}
+            />
+            <Text style={styles.modalText}>{character.title}</Text>
+            <Text style={styles.modalText}>{character.fullName}</Text>
+            <Text style={styles.modalText}>{character.family}</Text>
+            <Button
+              title="Close"
+              color="midnightblue"
+              onPress={() => setIsModalVisible(false)}
+            ></Button>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -44,5 +75,22 @@ const styles = StyleSheet.create({
     fontStyle: "italic",
     fontWeight: "600",
     color: "#B9B4C7",
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modal: {
+    padding: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "gray",
+    alignSelf: "center",
+    backgroundColor: "#000",
+  },
+  modalText: {
+    alignSelf: "center",
+    fontSize: 18,
   },
 });
